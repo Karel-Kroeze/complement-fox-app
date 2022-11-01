3cb71ab86795e600d9840466918023064c85662c
Karel A. Kroeze <k.a.kroeze@utwente.nl>
2022-11-01 11:27:39 +0100

    add allow-all CORS

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ import { handler } from './build/handler.js';
 
 const app = express();
 
+app.use((req, res, next) => {
+	// just reflect request headers back
+
+	res.header('Access-Control-Allow-Origin', req.headers.origin);
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,HEAD,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', req.headers['access-control-allow-headers']);
+
+	next();
+});
+
 // inject static server for messages
 app.use('/messages', express.static('./messages', { index: false }));
 
